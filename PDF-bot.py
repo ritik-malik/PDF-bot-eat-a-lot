3c79f738eaae0241d14dbfba658f6a1f69cec43e
ritik-malik <ritik18406@iiitd.ac.in>
2021-04-27 18:23:43 +0530

    added functions to generate wg-name and focus-areas md

--- PDF-bot.py
+++ PDF-bot.py
@@ -1,6 +1,8 @@
 # This is the main py script to generate automated PDF from metrics
 # It takes a YML file as an input (command line arg)
 #
+# THE SCRIPT IS NOT CLEAN RIGHT NOW! (for testing purposes only)
+#
 # Usage: python3 PDF-bot.py conf.yml
 
 import yaml
@@ -32,6 +34,7 @@ def generate_PDF(paths):
 
     print("Generated the PDF!\n")
 
+
 def generate_paths(values, paths):
 
     print("\nGenerating relative paths...\n")
@@ -43,12 +46,44 @@ def generate_paths(values, paths):
             cmd = 'cp ' + values['wg-name'] + '/focus-areas/' + focus + '/images/* images'
             os.system(cmd)
 
+            if metrics is not None:
+                gen_md_file(focus, 2)
+
             for metric in metrics:
+                # gen_md_file(metric, 3)    # no need for metrics heading
                 paths.append(values['wg-name'] + '/focus-areas/' + focus + '/' + metric)
         except:
             continue
     pprint(paths)
-    return paths
+    # return paths
+
+
+def gen_md_file(name, level):
+
+    # if name[len(name)-3:] != '.md':
+    #     name += '.md'
+
+    text = '#'*level + ' ' + name.upper()
+    name += '.md'
+
+    print("Generating file '{}' with text '{}'".format(name, text))
+
+
+    with open(name, 'w') as f:
+        f.write(text)
+
+    paths.append(name)
+
+
+def decrease_level(paths):
+
+    print("\nDecreasing heading levels by 2 in metric markdowns")
+
+    for i in paths:
+        if '/' in i:
+            cmd = 'sed -i "s/^\#/###/g" ' + i
+            os.system(cmd)
+
 
 
 ###### main()
@@ -84,10 +119,15 @@ for key, values in data.items():
         print("\nCloning '{}' from '{}' branch\n".format(key, values['github-branch']))
         subprocess.check_call(['git', 'clone', '-b', values['github-branch'], values['github-link'], key])
 
-        paths = generate_paths(values, paths)
+        gen_md_file(values['wg-name'], 1)
+
+        # paths =
+        generate_paths(values, paths)
 
     else:
         print('[WARNING]: Flag off for {}, ignoring this WG'.format(key))
+
+decrease_level(paths)
 
 generate_PDF(paths)
 
