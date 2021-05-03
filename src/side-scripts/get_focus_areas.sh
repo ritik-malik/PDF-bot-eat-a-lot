01a1d84a9508fa86c29caa85dff1f25665147ea3
ritik-malik <ritik18406@iiitd.ac.in>
2021-05-03 23:08:13 +0530

    fixed focus-areas table issue

--- src/side-scripts/get_focus_areas.sh
+++ src/side-scripts/get_focus_areas.sh
@@ -23,6 +23,18 @@ find . -size 0 -delete
 for f in outfile* 
 do
 	sed -i 's/###/##/' ${f}
+
+	# append the file with gfm version of html tables, via pandoc
+	# PS - extract only the table version, else it messes with headings, etc.
+	sed -n '/^<table>/,${p;/^<\/table>/q}' ${f} | pandoc --from html --to gfm >> ${f}
+
+	# remove the html table part
+	sed -i '/<div>/,/<\/div>/d' ${f}
+
+	# remove hyperlinks
+	sed -i 's/[][]\|([^()]*)//g' ${f}
+
+	# rename it with focus-area name
 	mv ${f} $(grep \#\# ${f} | cut -d - -f 2 | xargs | tr '[:upper:]' '[:lower:]' | sed 's/ /-/g').md
 
 done
